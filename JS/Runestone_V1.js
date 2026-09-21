754bf1ee92bdefd79454df77bdbe089f677fba3d
kiki-rgb-00 <331017962+kiki-rgb-00@users.noreply.github.com>
2026-09-20 20:03:37 -0400

    更新 Runestone_V1.js

--- JS/Runestone_V1.js
+++ JS/Runestone_V1.js
@@ -1200,7 +1200,7 @@ function main(config) {
       "behavior": "classical",
       "format": "yaml",
       "interval": 86400,
-      "url": "https://raw.githubusercontent.com/kiki-rgb-00/kiki/refs/heads/main/Rule/Advertising.yaml"
+      "url": "https://raw.githubusercontent.com/kiki-rgb-00/kiki/refs/heads/main/Rules/Advertising.yaml"
     },
 
     "Advertising_Domain": {
